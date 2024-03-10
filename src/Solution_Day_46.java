37ac6f7f45664dc04fd10e426b892ab1df7be483
maitrygajiwala <maitry22gajiwala@gmail.com>
2024-03-10 00:50:09 -0500

    Day 46 Linked List LeetCode 24

--- src/Solution_Day_46.java
+++ src/Solution_Day_46.java
@@ -1,31 +1,42 @@
 public class Solution_Day_46 {
 
-     // Definition for singly-linked list.
-     public class ListNode {
-          int val;
-          ListNode next;
-          ListNode() {}
-          ListNode(int val) { this.val = val; }
-          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     class Solution {
-        public ListNode swapPairs(ListNode head) {
-            if(head==null || head.next == null){
-                return head;
-            }
-            ListNode dummy = new ListNode(-1);
-            dummy.next = head;
-            ListNode curr = dummy;
-            while(curr.next!=null && curr.next.next!=null){
-                ListNode node1 = curr.next;
-                ListNode node2 = curr.next.next;
+    // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
 
-                node1.next = node2.next;
-                node2.next = node1;
-                curr.next = node2;
-                curr = node1;
+        ListNode() {
+        }
 
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        class Solution {
+            public ListNode swapPairs(ListNode head) {
+                if (head == null || head.next == null) {
+                    return head;
+                }
+                ListNode dummy = new ListNode(-1);
+                dummy.next = head;
+                ListNode curr = dummy;
+                while (curr.next != null && curr.next.next != null) {
+                    ListNode node1 = curr.next;
+                    ListNode node2 = curr.next.next;
+
+                    node1.next = node2.next;
+                    node2.next = node1;
+                    curr.next = node2;
+                    curr = node1;
+
+                }
+                return dummy.next;
             }
-            return dummy.next;
         }
     }
 }
