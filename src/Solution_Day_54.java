76ad96b96a25587edf219471fcd40bb20cf0e40f
maitrygajiwala <maitry22gajiwala@gmail.com>
2024-03-18 23:54:03 -0400

    Day 54 COMP 6481 Lab Solved Question

---
+++ src/Solution_Day_54.java
@@ -0,0 +1,23 @@
+public class Solution_Day_54 {
+    /* Had Lab today in which we were given to create a data structure which had
+    a singly linked list and a doubly linked list. The singly linked list consisted of controlling nodes
+    which were connected to two nodes from the doubly linked list.
+
+            10 ----> 90 ----> 32 ----> 23 ----> 21 ----> null
+           /  \     /  \     /  \     /  \     /  \
+          3 <->2<-> 4<-> 4<->3<->1<->3<->33<->33<->34<->null
+
+    Had to perform the following operations:
+    1.) Create this structure
+    2.) Display the elements in this form:
+        10 | 3 2
+        90 | 4 4
+        32 | 3 1
+        23 | 3 33
+        21 | 33 34
+    3.) addAtStart
+    4.) Find Parent Value
+    5.) Find Value of Parent Before my parent
+    6.) Insert a new node after my parent
+     */
+}
