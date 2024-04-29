c0a4a9f7f38c8897c772ddecda8d759ba63ca963
maitrygajiwala <maitry22gajiwala@gmail.com>
2024-04-28 23:13:31 -0400

    Day 95 Leetcode 258 Math

---
+++ src/Solution_Day_95.java
@@ -0,0 +1,19 @@
+public class Solution_Day_95 {
+    class Solution {
+        public int addDigits(int x) {
+            int sum=10;
+
+            while(sum/10 > 0){
+                sum = 0;
+                while(x!=0){
+                    sum+=x%10;
+                    x/=10;
+                }
+
+                x = sum;
+            }
+
+            return sum;
+        }
+    }
+}
